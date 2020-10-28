f160a082823c53640862605111e67a6871598e6e
夏永涛 <xiayt@happok.com>
2020-10-28 11:22:14 +0800

    增加声控

--- voiceCar.py
+++ voiceCar.py
@@ -12,6 +12,43 @@ import json
 GPIO.setmode(GPIO.BCM)
 
 
+class Speak:
+    def __init__ (self,message,times,launguage,isRemove):
+        '''
+        传入参数　消息　次数　是否清除合成的语音文件
+        '''
+        self.tts(message,launguage)
+        #说几次
+        for i in range(0,times):
+            self.say()
+        if isRemove:
+            self.over()
+    def tts(self,message,launguage):
+        try:
+            import requests
+        except:
+            print ("请下载python-requests模块后使用...")
+            exit(-1)
+        import urllib
+        s = requests.Session()
+        mes=''
+        if launguage is "zh":
+            mes="http://tts.baidu.com/text2audio?lan=zh&pid=101&vol=9&ie=UTF-8&text="
+        else:
+            mes="http://tts.baidu.com/text2audio?lan=en&pid=101&vol=9&ie=UTF-8&text="
+        s.get(mes+ urllib.quote(message))
+        res = s.get(mes+ urllib.quote(message)).content
+        f = open("tts-temp.mp3", "w")
+        f.write(res)
+        f.close()
+    def say(self):
+        os.system("play tts-temp.mp3")
+    def over(self):
+        os.system("rm tts-temp.mp3")
+    def play(music):
+        os.system("play "+music)
+
+# man=Speak("你好 中国",1,"zh",True)
 # 录音为wav格式音频文件，然后转换为pcm格式
 def makePCM():
     # 录音，"plughw:1,0"表示从USB话筒设备录音，这句命令最终录制完的格式就是百度要求的16k音质
